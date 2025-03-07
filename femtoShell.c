5bd7a9ad6786a11aced71bc5c95e367fd2a7d16b
Sheriefalgalaly <109813086+Sheriefalgalaly@users.noreply.github.com>
2025-03-07 18:33:41 +0200

    Add files via upload

---
+++ femtoShell.c
@@ -0,0 +1,54 @@
+#include <unistd.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	char *username;
+	if ((username = getlogin()) == NULL)
+		exit(0);
+	//	printf("%s",username);
+	char cmd[50] = {'\n'};
+	while (1)
+	{
+		printf("%s >", username);
+		int i = 0;
+		unsigned char ch = '#';
+
+		while (ch != '\n')
+		{
+			ch = (unsigned char)getchar();
+			cmd[i++] = ch;
+		}
+		cmd[i] = '\0';
+
+
+		// tokenize the input
+		char *token = strtok(cmd, " ");
+
+		if (!strcmp(token, "echo"))
+		{
+			
+			while (token != NULL)
+			{
+				token = strtok(NULL, " ");  // continue
+				if(token != NULL )printf("%s ", token);
+			}
+			printf("\n");
+		}
+		else if (!strcmp(token, "exit"))
+		{
+			printf("bye bye friend\n");
+			exit(0);
+		}
+		else
+		{
+			printf("invalid input\n");
+			exit(0);
+		}
+	}
+}
